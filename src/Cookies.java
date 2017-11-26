e72727e55ae167ce16b78b071e3dc9a53ecd3621
beeinei <beeinei@users.noreply.github.com>
2017-11-26 19:53:33 +0200

    first class

---
+++ src/Cookies.java
@@ -0,0 +1,29 @@
+public class Cookies {
+    private int size;
+    private String name;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Cookies(String n, int s) {
+        size = s;
+        name = n;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
