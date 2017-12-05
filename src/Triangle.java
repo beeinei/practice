2b943d1b84bf311944c6b51969510d95ec89879d
beeinei <beeinei@users.noreply.github.com>
2017-12-05 22:52:39 +0200

    מעבדה 6 - משימה 3 (שימוש מחלקה)

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Triangle {
     double a;
     double b;
@@ -41,6 +43,21 @@ public class Triangle {
         }
     }
 
-
+    private static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        boolean keepOn = true;
+        while (keepOn) {
+            System.out.println("Triangle 1! Enter 3 Edges");
+            Triangle t1 = new Triangle(scan.nextDouble(), scan.nextDouble(), scan.nextDouble());
+            System.out.println("Triangle 2! Enter 3 more Edges");
+            Triangle t2 = new Triangle(scan.nextDouble(), scan.nextDouble(), scan.nextDouble());
+            if (t1.similarTo(t2) == 0) {
+                System.out.println("The triangles are not similar. Try again!");
+            } else {
+                System.out.println("The triangles are similar. The ratio of similarity is: " + (t1.getA() / t2.getA()));
+                keepOn = false;
+            }
+        }
+    }
 }
 
